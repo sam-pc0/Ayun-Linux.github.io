d4d95847f826665b126be45ad01e10298dea22a7
sam_pc <sam_pc@tuta.io>
2020-02-02 20:42:05 -0600

    initial structure was created

---
+++ src/app.js
@@ -0,0 +1,20 @@
+// elements
+const app = document.getElementById("app");
+
+
+// classes
+
+
+
+// functions
+
+
+
+
+(function events(){ 
+
+	document.addEventListener('DOMContentLoaded', () =>{
+		console.warn("DOM Loaded");
+	});
+
+})();
