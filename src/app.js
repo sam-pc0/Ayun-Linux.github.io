c45dd9e0e2d9eef364b92d272f0693a3551259c3
sam_pc <sam_pc@tuta.io>
2020-02-03 22:40:53 -0600

    Assets was added and imported

--- src/app.js
+++ src/app.js
@@ -3,6 +3,26 @@ const app = document.getElementById("app");
 
 
 // classes
+class AssetsImport{
+
+	static async assetImport(path){
+		const response = await fetch(path);
+		let text = await response.text();
+		return text;
+	}
+
+	static async assetsLoad(){
+		const blueSections = [...document.getElementsByClassName("section-blue")];
+		const mainCurve = await this.assetImport("./assets/svg/main-curve.svg");
+		const topCurve= await this.assetImport("./assets/svg/top-curve.svg");
+		const bottomCurve= await this.assetImport("./assets/svg/bottom-curve.svg");
+
+		blueSections.forEach(section =>{
+			section.innerHTML += section.className.includes("features") ? mainCurve : topCurve;
+			section.innerHTML += bottomCurve;
+		});
+	}
+}
 
 
 
@@ -14,7 +34,7 @@ const app = document.getElementById("app");
 (function events(){ 
 
 	document.addEventListener('DOMContentLoaded', () =>{
-		console.warn("DOM Loaded");
+		AssetsImport.assetsLoad();
 	});
 
 })();
