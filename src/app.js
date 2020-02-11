aee806154375ca749a8c7fc692dc908b08f556f5
sam_pc <sam_pc@tuta.io>
2020-02-10 21:48:01 -0600

    header section was finished (mobile and desktop)

--- src/app.js
+++ src/app.js
@@ -1,26 +1,41 @@
 // elements
 const app = document.getElementById("app");
+const headerSection = document.getElementById("header");
 
 
 // classes
 class AssetsImport{
 
-	static async assetImport(path){
+	async assetImport(path){
 		const response = await fetch(path);
 		let text = await response.text();
 		return text;
 	}
 
-	static async assetsLoad(){
+	async loadCurves(){
 		const blueSections = [...document.getElementsByClassName("section-blue")];
-		const mainCurve = await this.assetImport("./assets/svg/main-curve.svg");
-		const topCurve= await this.assetImport("./assets/svg/top-curve.svg");
-		const bottomCurve= await this.assetImport("./assets/svg/bottom-curve.svg");
+		const mainCurve    = await this.assetImport("./assets/svg/main-curve.svg");
+		const topCurve     = await this.assetImport("./assets/svg/top-curve.svg");
+		const bottomCurve  = await this.assetImport("./assets/svg/bottom-curve.svg");
 
 		blueSections.forEach(section =>{
-			section.innerHTML += section.className.includes("features") ? mainCurve : topCurve;
+			section.innerHTML += section.className.includes("features") ? mainCurve: topCurve;
 			section.innerHTML += bottomCurve;
 		});
+
+		return true
+	}
+
+	async loadLogo(){
+
+	}
+
+	async assetsLoad(){
+		this.loadCurves();
+		const logo = await this.assetImport("./assets/svg/logo.svg");
+		const headerLogo = headerSection.getElementsByClassName("header__logo")[0];
+		let html = headerLogo.innerHTML;
+		headerLogo.innerHTML = logo + html;
 	}
 }
 
@@ -34,7 +49,7 @@ class AssetsImport{
 (function events(){ 
 
 	document.addEventListener('DOMContentLoaded', () =>{
-		AssetsImport.assetsLoad();
+		new AssetsImport().assetsLoad();
 	});
 
 })();
